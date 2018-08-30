5736b097bacdfa1f2aafb0c09cf4c9f31f5a6fe1
xdanieldzd <xdanieldzd@googlemail.com>
2018-08-30 08:16:41 +0200

    Changed argument handling, other stuff
    Command line arguments now somewhat smart, less hardcoded; also consolidated some other code, fixed missing character in SJIS/ASCII table

--- TitanMessage/Program.cs
+++ TitanMessage/Program.cs
@@ -5,14 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-using Newtonsoft.Json.Linq;
-
 namespace TitanMessage
 {
 	class Program
 	{
-		// import "D:\Translation\Etrian Odyssey 4 German\RomFS (Original EUR)\" "D:\Translation\Etrian Odyssey 4 German\JSON\"
-		// export "D:\Translation\Etrian Odyssey 4 German\JSON\" "D:\Translation\Etrian Odyssey 4 German\Generated Data\"
+		// --overwrite --json "D:\Translation\Etrian Odyssey 4 German\RomFS (Original EUR)\" "D:\Translation\Etrian Odyssey 4 German\JSON\" --binary "D:\Translation\Etrian Odyssey 4 German\JSON\" "D:\Translation\Etrian Odyssey 4 German\Generated Data\"
+
+		readonly static List<(string Long, string Short, string Description, string Syntax, Action<string[]> Method)> argumentHandlers = new List<(string Long, string Short, string Description, string Syntax, Action<string[]> Method)>()
+		{
+			{ ("json", "j", "Convert binary files to JSON files", "[source path] [target path]", ArgumentHandlerBinaryToJson) },
+			{ ("binary", "b", "Convert JSON files to binary files", null, ArgumentHandlerJsonToBinary) },
+			{ ("overwrite", "o", "Allow overwriting of existing files", null, (arg) => { overwriteExistingFiles = true; }) },
+		};
+
+		static bool overwriteExistingFiles = false;
+
+		static string applicationExecutable;
 
 		static void Main(string[] args)
 		{
@@ -21,130 +29,149 @@ namespace TitanMessage
 			Console.WriteLine();
 
 			args = CommandLineTools.CreateArgs(Environment.CommandLine);
+			applicationExecutable = Path.GetFileName(args[0]);
 
-			if (args.Length != 4)
-				PrintUsageAndExit(-1);
-
-			try
+			if (args.Length <= 1)
 			{
-				var mode = args[1];
-				var sourceRoot = new DirectoryInfo(args[2]);
-				var targetRoot = new DirectoryInfo(args[3]);
-
-				switch (mode.ToLowerInvariant())
-				{
-					case "import":
-						// Special cases: include only known valid TBLs, exclude known incompatible leftovers (TestData folder, EO3 seafaring, etc...)
-						var importFiles = sourceRoot.EnumerateFiles("*", SearchOption.AllDirectories)
-							.Where(x => (x.Extension == ".tbl" && (x.Name.EndsWith("nametable.tbl") || x.Name == "skyitemname.tbl")) || x.Extension == ".mbm")
-							.Where(x => !x.DirectoryName.Contains("TestData") && !x.Name.StartsWith("sea") && !x.Name.StartsWith("FacilityEntranceText"));
-
-						foreach (var importFile in importFiles)
-						{
-							var relative = importFile.DirectoryName.Replace(sourceRoot.FullName, string.Empty).TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
-							ProcessFile(importFile.FullName, targetRoot.FullName, relative);
-						}
-						break;
-
-					case "export":
-						var exportFiles = sourceRoot.EnumerateFiles("*.json", SearchOption.AllDirectories);
-						foreach (var exportFile in exportFiles)
-						{
-							var relative = exportFile.DirectoryName.Replace(sourceRoot.FullName, string.Empty).TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
-							ProcessFile(exportFile.FullName, targetRoot.FullName, relative);
-						}
-						break;
-
-					default:
-						throw new Exception($"Unknown conversion mode '{mode}'");
-				}
-
+				Console.WriteLine("No arguments specified!");
 				Console.WriteLine();
-				Console.WriteLine("Press any key to exit.");
-				Console.ReadKey();
+				PrintUsageAndExit(-1);
 			}
-			catch (Exception ex)
+
+			foreach (var argGroup in ParseArguments(args))
 			{
-				Console.WriteLine($"Exception occured: {ex.Message}");
-				Console.WriteLine();
-				PrintUsageAndExit(-1);
+				try
+				{
+					argumentHandlers.FirstOrDefault(x => x.Long == argGroup[0] || x.Short == argGroup[0]).Method?.Invoke(argGroup);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Exception occured: {ex.Message}");
+					Console.WriteLine();
+					PrintUsageAndExit(-1);
+				}
+			}
+		}
+
+		static List<string[]> ParseArguments(string[] args)
+		{
+			var argGroups = new List<string[]>();
+			for (int argIdx = 1; argIdx < args.Length; argIdx++)
+			{
+				if (args[argIdx].StartsWith("-"))
+				{
+					var argGroup = new List<string> { args[argIdx].TrimStart('-') };
+					argGroup.AddRange(args.Skip(argIdx + 1).TakeWhile(x => !x.StartsWith("-")));
+					argGroups.Add(argGroup.ToArray());
+					argIdx += (argGroup.Count - 1);
+				}
+			}
+			return argGroups;
+		}
+
+		static string GetRelativePath(FileInfo sourceFile, DirectoryInfo sourceRoot)
+		{
+			return sourceFile.DirectoryName.Replace(sourceRoot.FullName, string.Empty).TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+		}
+
+		static bool CheckOkayToContinue(FileInfo outputFileInfo)
+		{
+			if (outputFileInfo.Exists && !overwriteExistingFiles)
+			{
+				Console.WriteLine($"File {outputFileInfo.Name} already exists, skipping...");
+				return false;
+			}
+			else
+				return true;
+		}
+
+		static void ArgumentHandlerBinaryToJson(string[] args)
+		{
+			if (args.Length != 3) throw new Exception($"Invalid number of arguments for {args[0]}");
+
+			var sourceRoot = new DirectoryInfo(args[1]);
+			var targetRoot = new DirectoryInfo(args[2]);
+
+			// Special cases: include only known valid TBLs, exclude known incompatible leftovers (TestData folder, EO3 seafaring, etc...)
+			var binaryFiles = sourceRoot.EnumerateFiles("*", SearchOption.AllDirectories)
+				.Where(x => (x.Extension == ".tbl" && (x.Name.EndsWith("nametable.tbl") || x.Name == "skyitemname.tbl")) || x.Extension == ".mbm")
+				.Where(x => !x.DirectoryName.Contains("TestData") && !x.Name.StartsWith("sea") && !x.Name.StartsWith("FacilityEntranceText"));
+
+			foreach (var binaryFile in binaryFiles)
+			{
+				var relativePath = GetRelativePath(binaryFile, sourceRoot);
+				var outputFileInfo = new FileInfo(Path.Combine(targetRoot.FullName, relativePath, $"{Path.GetFileNameWithoutExtension(binaryFile.FullName)}.json"));
+
+				if (CheckOkayToContinue(outputFileInfo))
+				{
+					Console.WriteLine($"Converting binary {binaryFile.Name} to JSON...");
+
+					outputFileInfo.Directory.Create();
+
+					Translation translationFile;
+					switch (binaryFile.Extension)
+					{
+						case ".tbl": translationFile = StringTableHandler.ImportBinary(binaryFile.FullName, relativePath); break;
+						case ".mbm": translationFile = MessageBinaryHandler.ImportBinary(binaryFile.FullName, relativePath); break;
+						default: throw new Exception($"Unrecognized file extension {binaryFile.Extension}");
+					}
+					translationFile.SerializeToFile(outputFileInfo.FullName);
+				}
+			}
+		}
+
+		static void ArgumentHandlerJsonToBinary(string[] args)
+		{
+			if (args.Length != 3) throw new Exception($"Invalid number of arguments for {args[0]}");
+
+			var sourceRoot = new DirectoryInfo(args[1]);
+			var targetRoot = new DirectoryInfo(args[2]);
+
+			var jsonFiles = sourceRoot.EnumerateFiles("*.json", SearchOption.AllDirectories);
+
+			foreach (var jsonFile in jsonFiles)
+			{
+				Translation translationFile = jsonFile.FullName.DeserializeFromFile<Translation>();
+
+				var relativePath = GetRelativePath(jsonFile, sourceRoot);
+				var outputFileInfo = new FileInfo(Path.Combine(targetRoot.FullName, translationFile.RelativePath));
+
+				if (CheckOkayToContinue(outputFileInfo))
+				{
+					Console.WriteLine($"Converting JSON {jsonFile.Name} to binary...");
+
+					outputFileInfo.Directory.Create();
+
+					switch (translationFile.FileType)
+					{
+						case StringTableHandler.FileType: StringTableHandler.ExportBinary(translationFile, outputFileInfo.FullName); break;
+						case MessageBinaryHandler.FileType: MessageBinaryHandler.ExportBinary(translationFile, outputFileInfo.FullName); break;
+						default: throw new Exception($"Unrecognized translation type {translationFile.FileType}");
+					}
+				}
 			}
 		}
 
 		static void PrintUsageAndExit(int code)
 		{
-			Console.WriteLine("Usage: [import | export] <source path> <target path>");
+			Console.WriteLine($"Usage: {applicationExecutable} [options]...");
 			Console.WriteLine();
-			Console.WriteLine("  import        Convert TBL/MBM to JSON");
-			Console.WriteLine("  export        Convert JSON to TBL/MBM");
-			Console.WriteLine("  source path   Path to source files to be converted");
-			Console.WriteLine("  target path   Path to write converted files to");
-			Console.WriteLine();
-			Console.WriteLine("Example: TitanMessage.exe import \"C:\\EO4\\RomFS\\\" \"C:\\EO4\\JSON\\\"");
-			Console.WriteLine();
-			Console.WriteLine("Use 'import' OR 'export' mode. Directory structure is preserved.");
+			Console.WriteLine("Options:");
+
+			var maxSpecifierLength = argumentHandlers.Select(x => $" {x.Short}{(x.Long != string.Empty ? "," : " ")} {x.Long}").Max(x => x.Length);
+			foreach (var (Long, Short, Description, Syntax, _) in argumentHandlers)
+			{
+				var specifierString = $"{Short}{(Long != string.Empty ? "," : " ")} {Long}";
+				var padding = string.Empty.PadRight((maxSpecifierLength + 3) - specifierString.Length, ' ');
+				Console.WriteLine($" {specifierString}{padding}{Description}");
+				if (!string.IsNullOrWhiteSpace(Syntax))
+					Console.WriteLine($"{new string(' ', specifierString.Length)}{padding}  [{specifierString.Replace(", ", "|")}] {Syntax}");
+			}
 			Console.WriteLine();
 			Console.WriteLine("Press any key to exit.");
 			Console.ReadKey();
 
 			Environment.Exit(code);
-		}
-
-		static void ProcessFile(string sourceFileName, string targetRoot, string relativePath)
-		{
-			string outputFullName = string.Empty;
-			Translation translationFile = null;
-
-			switch (Path.GetExtension(sourceFileName))
-			{
-				case ".tbl":
-				case ".mbm":
-					outputFullName = Path.Combine(targetRoot, relativePath, $"{Path.GetFileNameWithoutExtension(sourceFileName)}.json");
-					break;
-
-				case ".json":
-					translationFile = sourceFileName.DeserializeFromFile<Translation>();
-					outputFullName = Path.Combine(targetRoot, translationFile.RelativePath);
-					break;
-			}
-
-			if (File.Exists(outputFullName))
-			{
-				Console.WriteLine($"File {Path.GetFileName(outputFullName)} already exists, skipping...");
-				return;
-			}
-
-			Directory.CreateDirectory(Path.GetDirectoryName(outputFullName));
-
-			switch (Path.GetExtension(sourceFileName))
-			{
-				case ".tbl":
-					Console.WriteLine($"Converting {Path.GetFileName(sourceFileName)} to JSON...");
-					translationFile = StringTableHandler.ImportBinary(sourceFileName, relativePath);
-					translationFile.SerializeToFile(outputFullName);
-					break;
-
-				case ".mbm":
-					Console.WriteLine($"Converting {Path.GetFileName(sourceFileName)} to JSON...");
-					translationFile = MessageBinaryHandler.ImportBinary(sourceFileName, relativePath);
-					translationFile.SerializeToFile(outputFullName);
-					break;
-
-				case ".json":
-					switch (translationFile.FileType)
-					{
-						case StringTableHandler.FileType:
-							Console.WriteLine($"Converting {Path.GetFileName(sourceFileName)} to TBL...");
-							StringTableHandler.ExportBinary(translationFile, outputFullName);
-							break;
-
-						case MessageBinaryHandler.FileType:
-							Console.WriteLine($"Converting {Path.GetFileName(sourceFileName)} to MBM...");
-							MessageBinaryHandler.ExportBinary(translationFile, outputFullName);
-							break;
-					}
-					break;
-			}
 		}
 	}
 }
